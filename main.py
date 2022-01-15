c4b0adf6cd72567bfab3bdeeb336b29be09ed3c0
DashaPyshnenko <71897811+DashaPyshnenko@users.noreply.github.com>
2022-01-15 16:46:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import nltk
 import pymorphy2
 from nltk.parse.dependencygraph import DependencyGraph
 text = 'Наступило лето. В саду поспела смородина. Даша и Алиса собирают ее в ведерко. Мама будет варить варенье. Дети будут пить чай с вареньем.'
-sentences = re.compile(r'[.|!|?|…]').split(text.lower())
+sentences = re.compile(r'[.|!|?|…]').split(text.lower()) #разделяем текст на предложения
 print(sentences)
 
-tokens = nltk.word_tokenize(text.lower(), language="russian")
-tagged = nltk.pos_tag(tokens)
+tokens = nltk.word_tokenize(text.lower(), language="russian") #делим на слова и выполняем токенизацию
+tagged = nltk.pos_tag(tokens) 
 print(tagged)
 morph = pymorphy2.MorphAnalyzer(lang='ru')
 gramm = {}
+#определяем части речи
 for word in tokens:
     p = morph.parse(word)[0]
     if word not in gramm:
         gramm[word] = p.tag
 print(gramm)
-
+#предполагаем структуру предложений
 grammar_all = nltk.CFG.fromstring("""
    S -> NP VP | VP NP
    VP -> V | V N | V N PP | V PP | V PP N | PP V | V V | V S
@@ -27,6 +28,7 @@ grammar_all = nltk.CFG.fromstring("""
    P -> "у" | "в" | "и" | "из" | "с" 
    """)
 i = 0
+#строим дерево структуры
 while i < len(sentences):
     structure = nltk.word_tokenize(sentences[i], language='russian')
     parser = nltk.RecursiveDescentParser(grammar_all)
